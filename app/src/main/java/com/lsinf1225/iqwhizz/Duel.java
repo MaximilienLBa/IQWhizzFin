c388a8a21ef0a1e4ef0723f79ee2c22008de1493
mdraguet <49276735+mdraguet@users.noreply.github.com>
2019-05-07 11:22:34 +0200

    Add files via upload

---
+++ app/src/main/java/com/lsinf1225/iqwhizz/Duel.java
@@ -0,0 +1,46 @@
+package com.lsinf1225.iqwhizz;
+
+public class Duel {
+    private int id1;
+    private int id2;
+    private float score1;
+    private float score2;
+
+
+    public void setScore1(){
+        this.score1=score1;
+    }
+
+    public void setScore2(){
+        this.score2=score2;
+    }
+
+    public float getScore1() {
+        return score1;
+    }
+
+    public float getScore2() {
+        return score2;
+    }
+
+    public void setId1() {
+        this.id1 = id1;
+    }
+
+    public void setId2() {
+        this.id2 = id2;
+    }
+
+    public int getId1() {
+        return id1;
+    }
+
+    public int getId2() {
+        return id2;
+    }
+
+
+
+
+
+}
